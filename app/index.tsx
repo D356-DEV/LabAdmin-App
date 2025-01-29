316f42890574b3772a6f2ec4cc87e8aab5d59d16
Diego Baez <d356developer@gmail.com>
2025-01-29 14:15:23 -0600

    Added log in screen and logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput, Button} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+
+        <View style={styles.logoContainer}>
+          <Image source={require('@/assets/images/icon.png')} style={styles.logo} />
+          <Text style={styles.title}>LabAdmin</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+          <TextInput style={styles.inputForm} placeholder="Identifier" placeholderTextColor="#fff"/>
+          <TextInput style={styles.inputForm} placeholder="Password" placeholderTextColor="#fff" secureTextEntry />
+          <Button
+            title="Log In"
+            color="orange"
+          />
+        </View>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: "#f5f5f5",
+    justifyContent: 'center'
+  },
+  logoContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  formContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 15,
+  },
+  inputForm: {
+    backgroundColor: "orange",
+    width: "90%",
+    height: 50,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    color: "#fff",
+    fontSize: 16,
+  },
+});
